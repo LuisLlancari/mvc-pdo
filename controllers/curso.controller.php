6a28977c3f2d8aa79bd1708c318837ec3ea41771
Luis Llancari <miguel.llancari12@gmail.com>
2023-04-04 16:20:31 -0500

    eliminar mvc-pdo

---
+++ controllers/curso.controller.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once '../models/Curso.php';
+
+if (isset($_POST['operacion'])){
+
+  $curso = new Curso();
+
+  if ($_POST['operacion'] == 'listar'){
+
+    $datosObtenidos = $curso->listarCursos();
+    
+    //En esta ocasión NO enviaremos un objeto JSON, en su lugar
+    //el controlador renderizará las filas que necesita <tbody></tbody>
+    //echo json_encode($datosObtenidos);
+
+    //PASO 1: Verificar que el objeto contenga datos
+    if ($datosObtenidos){
+      $numeroFila = 1;
+      //PASO 2: Recorrer todo el objeto  
+      foreach($datosObtenidos as $curso){
+        //PASO 3: Ahora construimos las filas
+        echo "
+          <tr>
+            <td>{$numeroFila}</td>
+            <td>{$curso['nombrecurso']}</td>
+            <td>{$curso['especialidad']}</td>
+            <td>{$curso['complejidad']}</td>
+            <td>{$curso['fechainicio']}</td>
+            <td>{$curso['precio']}</td>
+            <td>
+              <a href='#' data-idcurso='{$curso['idcurso']}' class='btn btn-danger btn-sm eliminar'><i class='bi bi-trash3'></i></a>
+              <a href='#' data-idcurso='{$curso['idcurso']}' class='btn btn-info btn-sm editar'><i class='bi bi-pencil-fill'></i></a>
+            </td>
+          </tr>
+        ";
+        $numeroFila++;
+      }
+    }
+
+  }
+
+  if ($_POST['operacion'] == 'registrar'){
+    //paso 1. Recoger los datos que nos envia la vista (FORM, utilizando AJAX)
+    //$_post: Esto es lo que no envia desde FORM
+    $datosForm = [
+      "nombrecurso"   => $_POST['nombrecurso'],
+      "especialidad"  => $_POST['especialidad'],
+      "complejidad"   => $_POST['complejidad'],
+      "fechainicio"   => $_POST['fechainicio'],
+      "precio"        => $_POST['precio'],
+    ];
+
+    //paso 2: Enviar arreglo con el prarmetro
+    $curso->registrarCurso($datosForm);
+  }
+
+  if ($_POST['operacion'] == 'eliminar'){
+    $curso->eliminarCurso($_POST['idcurso']);
+  }  
+}
---
+++ models/Curso.php
@@ -0,0 +1,75 @@
+<?php
+
+require_once "Conexion.php";
+
+//MODELO = CONTIENE LA LÓGICA
+//extends : HERENCIA (POO) en PHP
+class Curso extends Conexion{
+
+  //Objeto que almacena la conexión que viene desde el padre(Conexion)
+  //y la compartirá con todos los métodos (CRUD+)
+  private $accesoBD;
+
+  //Constructor, INICIALIZAR
+  public function __CONSTRUCT(){
+    $this->accesoBD = parent::getConexion();
+  }
+
+  //Método listar cursos
+  public function listarCursos(){
+    try{
+      //1. Preparamos la consulta
+      $consulta = $this->accesoBD->prepare("CALL spu_cursos_listar()");
+      //2. Ejecutamos la consulta
+      $consulta->execute();
+      //3. Devolvemos el resultado (array asociativo)
+      return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+
+  public function registrarCurso($datos = []){
+    try{
+      // 1. Preparamos la consulta
+      $consulta = $this->accesoBD->prepare("CALL spu_cursos_insertar(?,?,?,?,?)");
+      // 2. ejecutamos la consulta
+      $consulta->execute(
+        array(
+          $datos["nombrecurso"],
+          $datos["especialidad"],
+          $datos["complejidad"],
+          $datos["fechainicio"],
+          $datos["precio"]
+          )
+      );
+    }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+
+  
+  public function eliminarCurso($idcurso = 0){
+    try{
+      $consulta = $this->accesoBD->prepare("CALL spu_cursos_eliminar(?)");
+      $consulta->execute(array($idcurso));
+    }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+
+  public function actualizarCurso(){
+    try{
+
+    }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+
+}
+
+?>
